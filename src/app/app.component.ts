05db1a7a041605488aa4553bb95b4a55a04fd314
bhairabpatra <bhairabpatra.iitd@gmail.com>
2022-01-22 03:09:56 +0530

    code update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ReadingTimePipe } from './_pipes/reading-time.pipe';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-pipes-implements';
+  content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos est aperiam maxime dicta voluptate? Pariatur sint tempora commodi autem vero repellat voluptates ut dolorum eaque consequuntur accusantium reiciendis, dignissimos enim deserunt recusandae doloribus iusto ex illum ducimus corporis! Dolor quisquam voluptate commodi nostrum, odio minima perspiciatis sint dolorum soluta nulla."
 }
